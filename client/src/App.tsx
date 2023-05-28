59ecc6d1a5b3adcc5e3204795052ca28fa90c63b
mikaelsouza-dev <mikael.silva62@aluno.ce.gov.br>
2023-05-28 09:28:29 -0300

    added a pages and react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,40 @@
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Single from './pages/Single';
+import Write from './pages/Write';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+]);
 
 function App() {
 
   return (
-    <p>initial configs</p>
+    <RouterProvider router={router}/>
   )
 }
 
